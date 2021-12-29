43670f6b8078538c5350848c729c9906bd0f96bd
jajo21 <89775394+jajo21@users.noreply.github.com>
2021-12-29 10:26:18 +0100

    Har gjort en början med mockup för att visa inventarier. Vidare ska funktioner för att lägga till inventarier i databas sättas upp.

---
+++ Auktioner/Models/ArticleRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Auktioner.Models
+{
+    public class ArticleRepository : IArticleRepository
+    {
+        private readonly AppDbContext _appDbContext;
+        public ArticleRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public IEnumerable<Article> AllArticles
+        {
+            get
+            {
+                return _appDbContext.Articles;
+            }
+        }
+        public IEnumerable<Article> ArticlesInStock()
+        {
+            return _appDbContext.Articles.Where(a => a.Sold == false);
+        }
+    }
+}
---
+++ Auktioner/ViewModels/InventoryListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Auktioner.Models;
+
+namespace Auktioner.ViewModels
+{
+    public class InventoryListViewModel
+    {
+        IEnumerable<Article> articles;
+        public IEnumerable<Article> Articles
+        {
+            get { return this.articles; }
+            set { this.articles = value; }
+        }
+    }
+}
