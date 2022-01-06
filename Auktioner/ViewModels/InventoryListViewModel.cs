1afc91317f9026883a657ee5b9c61225d71eb054
Johannes Jakobsson <jajo21pg@student.ju.se>
2022-01-06 15:32:05 +0100

    Fixade properties

--- Auktioner/ViewModels/InventoryListViewModel.cs
+++ Auktioner/ViewModels/InventoryListViewModel.cs
@@ -5,11 +5,6 @@ namespace Auktioner.ViewModels
 {
     public class InventoryListViewModel
     {
-        IEnumerable<AuctionItem> auctionItems;
-        public IEnumerable<AuctionItem> AuctionItems
-        {
-            get { return this.auctionItems; }
-            set { this.auctionItems = value; }
-        }
+        public IEnumerable<AuctionItem> AuctionItems {get; set;}
     }
 }
